0a431d4df1f23051c881aa7adac701f13d7e4191
yusiang <10656010@ntub.edu.tw>
2021-09-13 09:01:31 +0800

    新增 - 學生預覽、下載

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const mModule = require("./util/module")
-const util =require("./util/util")
+const util = require("./util/util")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,19 +10,19 @@ router.get('/', function (req, res, next) {
 });
 
 // 登入
-router.post('/', async(req, res, next)=> {
+router.post('/', async (req, res, next) => {
   // console.log(req.body )
-  data=await mModule.login(req.body)
+  data = await mModule.login(req.body)
   console.log(data)
-  let msg=data>0?"登入成功":"登入失敗"
-  res.send(util.ret(data>0,msg))
+  let msg = data.isLogin ? "登入成功" : "登入失敗"
+  res.send(util.ret(data.isLogin, msg, data.d))
 });
 
 
 // 下載檔案
-router.get('/download/:fileName', function(req, res){
+router.get('/download/:fileName', function (req, res) {
   // console.log("enter download",path.join(__dirname,"../uploads/",req.params.fileName))  
-  var file = path.join(__dirname,"../uploads/",req.params.fileName) 
+  var file = path.join(__dirname, "../uploads/", req.params.fileName)
   res.download(file);
 });
 
